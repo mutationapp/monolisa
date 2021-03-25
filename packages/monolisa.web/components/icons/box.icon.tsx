a021426cf33ac56ba522463a4f8ed2dd46f2443f
Ibrahim Velinov <ibrahimvelinov@Ibrahims-MacBook-Pro.local>
2021-03-25 17:31:48 +0100

    🚀

--- packages/monolisa.web/components/icons/box.icon.tsx
+++ packages/monolisa.web/components/icons/box.icon.tsx
@@ -6,18 +6,18 @@ const BoxIcon: IconType = props => {
   return (
     <svg
       viewBox="0 0 24 24"
-      width="1em"
-      height="1em"
+      width={24}
+      height={24}
       stroke="currentColor"
       strokeWidth={1.5}
       strokeLinecap="round"
       strokeLinejoin="round"
       fill="none"
       shapeRendering="geometricPrecision"
+      color="var(--geist-foreground)"
       {...props}
     >
-      <path d="M21 16V8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z" />
-      <path d="M3.27 6.96L12 12.01l8.73-5.05M12 22.08V12" />
+      <rect x={3} y={3} width={18} height={18} rx={2} ry={2} />
     </svg>
   )
 }
