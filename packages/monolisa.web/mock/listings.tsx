4b813ded5fb06cf1292bfa50032b93780df1bdaf
ibsukru <ibsukru@gmail.com>
2021-03-30 21:10:46 +0200

    🚀

--- packages/monolisa.web/mock/listings.tsx
+++ packages/monolisa.web/mock/listings.tsx
@@ -3,25 +3,6 @@ import { randomEmoji } from './emoji'
 import { v4 as uuid } from 'uuid'
 
 const listings = [
-  {
-    id: uuid(),
-    content: '**Thus Spoke Zarathustra**',
-    createdDate: date.past(),
-    company: {
-      slug: 'diplomaticTechno',
-      picture: randomEmoji(),
-    },
-  },
-  {
-    id: uuid(),
-    createdDate: date.past(),
-    content:
-      'As a **Senior** WordPress Developer, at -Awesome- Motive, you’ll join the WPFormsteam where you will collaborate with awesome team members and tackle a wide range of responsibilities.',
-    company: {
-      slug: 'diplomaticTechno',
-      picture: '🍇',
-    },
-  },
   ...new Array(20).fill(1).map(() => {
     return {
       id: uuid(),
