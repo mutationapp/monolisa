edbcd8e4ee095841913d3f776e399dca8cab04a7
ibsukru <ibsukru@gmail.com>
2021-03-30 20:58:15 +0200

    🚀

--- packages/monolisa.web/pages/index.tsx
+++ packages/monolisa.web/pages/index.tsx
@@ -1,8 +1,84 @@
-import { Setup } from '../components'
+import { Box, Button, Markdown } from '../components'
 import React from 'react'
+import { listingType } from './jobs/list'
+import listingsMock from '../mock/listings'
+import { css, t } from '../styles/typography'
+import Link from 'next/link'
+import { DashboardLayout } from '../components/layouts'
 
 function HomePage() {
-  return <Setup />
+  const [jobs] = React.useState<listingType[]>(listingsMock)
+
+  return (
+    <DashboardLayout
+      heading={<h1>Automated job board for github organizations</h1>}
+      pull
+      aside={' '}
+      back={true}
+    >
+      <ul
+        className={css({
+          display: 'flex',
+          flexDirection: 'column',
+          margin: '-25px 0',
+          gap: '15px',
+        })}
+      >
+        {jobs.map(job => {
+          const { company } = job
+
+          return (
+            <Box
+              shadow
+              key={job.id}
+              footer={
+                <div
+                  className={css({
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: '100%',
+                  })}
+                >
+                  <div className={css({ flex: 1 })}>
+                    @
+                    <Link {...{ href: `/teams/mutationapp` }}>
+                      <a
+                        className={css({
+                          marginLeft: 2,
+                        })}
+                      >
+                        {company.slug}
+                      </a>
+                    </Link>
+                    , 23.10.1984
+                  </div>
+                  <Button link={{ ...{ href: `/jobs/${job.id}` } }}>
+                    Job Details
+                  </Button>
+                </div>
+              }
+            >
+              <div
+                className={css({
+                  position: 'relative',
+                  top: '-10px',
+                  left: '-10px',
+                })}
+              >
+                {/* <CompanyProfile {...{ listing: tweet }} /> */}
+              </div>
+              <section>
+                <div style={t.content} className="tweet-details-content">
+                  <Markdown>{job.content}</Markdown>
+                </div>
+              </section>
+            </Box>
+          )
+        })}
+      </ul>
+    </DashboardLayout>
+  )
 }
 
 export default HomePage
